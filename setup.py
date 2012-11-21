972e5c373e72c1aa525a4745487f8ef995a6050e
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2012-11-21 16:14:37 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+setup(
+    author = "Badi' Abdul-Wahid",
+    author_email = 'abdulwahidc@gmail.com',
+    url = 'https://github.com/badi/pymail',
+    name = 'pymail',
+    scripts = ['pymail'],
+    platforms = ['Linux', 'Mac OS X'],
+    description = 'A commandline utility for sending emails, optionally with html and/or attachments'
+    )
